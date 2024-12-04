2c4e96e2d57eb381940bb40c5f67b9b9799b8519
kasahara-san <kasahara.yuichiro.res@gmail.com>
2024-12-04 14:16:46 +0900

    Update: 一部のプログラムを整理

--- launch/demo202412.launch.py
+++ launch/demo202412.launch.py
@@ -8,14 +8,16 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    ros_tcp_endpoint_dir = get_package_share_directory('ros_tcp_endpoint')
     ic120_unity_dir = get_package_share_directory('ic120_unity')
     zx200_unity_dir = get_package_share_directory('zx200_unity')
     tms_ts_launch_dir = get_package_share_directory('tms_ts_launch')
     simizu_project_dir = get_package_share_directory('simizu_project')
     tms_if_for_opera_dir = get_package_share_directory('tms_if_for_opera')
+    ros_tcp_endpoint_launch_file = os.path.join(ros_tcp_endpoint_dir, 'launch', 'endpoint.py')
     ic120_standby_ekf_launch_file = os.path.join(ic120_unity_dir, 'launch', 'ic120_standby_ekf.launch.py')
     zx200_standby_launch_file = os.path.join(zx200_unity_dir, 'launch', 'zx200_standby.launch.py')
-    sample_tms_ts_construction_launch_file = os.path.join(tms_ts_launch_dir, 'launch', 'sample_tms_ts_construction.launch.py')
+    tms_ts_demo202412_launch_file = os.path.join(tms_ts_launch_dir, 'launch', 'tms_ts_demo202412.launch.py')
     tms_if_for_opera_launch_file = os.path.join(tms_if_for_opera_dir, 'launch', 'tms_if_for_opera.launch.py')
     sample_rviz_file = os.path.join(simizu_project_dir, 'rviz2', 'sample.rviz')
     return LaunchDescription([
@@ -26,6 +28,11 @@ def generate_launch_description():
         DeclareLaunchArgument('task_id1', default_value='8'),
         DeclareLaunchArgument('task_id2', default_value='9'),
 
+        # ros_tcp_endpoint
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(ros_tcp_endpoint_launch_file),
+        ),
+        
         # IC120用のナビゲーション用
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(ic120_standby_ekf_launch_file),
@@ -52,7 +59,7 @@ def generate_launch_description():
 
         # ROS2-TMS for Construction用
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(sample_tms_ts_construction_launch_file),
+            PythonLaunchDescriptionSource(tms_ts_demo202412_launch_file),
             launch_arguments={
                 'task_id1': launch.substitutions.LaunchConfiguration('task_id1'),
                 'task_id2': launch.substitutions.LaunchConfiguration('task_id2')
